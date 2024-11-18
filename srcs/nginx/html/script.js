ac4af2146ef065fd95d647f713d53540c13dd2f7
valyriasteel <beratbosnak@hotmail.com>
2024-11-18 21:45:06 +0300

    bootstrap added

--- srcs/nginx/html/script.js
+++ srcs/nginx/html/script.js
@@ -1,8 +1,15 @@
 function showPage(pageId) {
     // Tüm sayfaları gizle
     const pages = document.querySelectorAll('.page');
-    pages.forEach(page => page.classList.add('hidden'));
+    pages.forEach(page => page.classList.add('d-none'));
 
     // Seçilen sayfayı göster
-    document.getElementById(pageId).classList.remove('hidden');
+    document.getElementById(pageId).classList.remove('d-none');
+
+    // Navbar menüsünü mobilde kapat
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    if (navbarToggler && navbarCollapse.classList.contains('show')) {
+        navbarToggler.click();
+    }
 }
