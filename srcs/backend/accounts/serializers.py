336a063b30aab6cf86e27b10f26a1bc40d360a8f
valyriasteel <beratbosnak@hotmail.com>
2024-12-23 04:41:27 -0800

    api update

--- srcs/backend/accounts/serializers.py
+++ srcs/backend/accounts/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import UserCreateProfile
 
-class UserCreateProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserCreateProfile
-        fields = ['avatar', 'username', 'name', 'surname', 'email']
+class Verify2FASerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    code = serializers.RegexField(regex=r'^\d{6}$', max_length=6)
+    token = serializers.CharField()
